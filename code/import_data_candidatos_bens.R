e6c5a18f5393f9fcaa9cc6155b6ff7d0a2d3830d
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2018-04-02 22:28:57 -0300

    Merge história de cabedelo do branch

--- code/import_data_candidatos_bens.R
+++ code/import_data_candidatos_bens.R
@@ -1,46 +1,83 @@
 library(readr)
 library(dplyr)
 
+library(readr)
+library(dplyr)
+
+importDecalaracao2008 <- function(dataPath) {
+    declaracao_2008 <- read_delim(dataPath, delim = ";", col_names = FALSE, col_types = "cciccciccncc",
+                                  locale = locale(encoding = "latin1")) 
+    
+    colunas_declaracao <- c("dataGeracao", "horaGeracao", "anoEleicao", "descEleicao", "siglaUF", 
+                            "sequencialCandidato", "codTipoBem", "descricaoTipoBem", 
+                            "detalheBem", "valorBem", "dataUltimaAtualizacao", "horaUltimaAtualizacao")
+    
+    colnames(declaracao_2008) <- colunas_declaracao
+    
+    return(declaracao_2008)
+}
+
+
 importDecalaracao2012 <- function(dataPath){
-  declaracao_2012 <- read_delim(dataPath, delim = ";", col_names = FALSE, col_types = "cciccciccncc",
-                                locale = locale(encoding = "latin1"))
-  
-  colunas_declaracao <- c("dataGeracao", "horaGeracao", "anoEleicao", "descEleicao", "siglaUF", "sequencialCandidato", "codTipoBem", "descricaoTipoBem", 
-                          "detalheBem", "valorBem", "dataUltimaAtualizacao", "horaUltimaAtualizacao")
-  
-  colnames(declaracao_2012) <- colunas_declaracao
-  
-  return(declaracao_2012)
+    declaracao_2012 <- read_delim(dataPath, delim = ";", col_names = FALSE, col_types = "cciccciccncc",
+                                  locale = locale(encoding = "latin1"))
+    
+    colunas_declaracao <- c("dataGeracao", "horaGeracao", "anoEleicao", "descEleicao", "siglaUF", "sequencialCandidato", "codTipoBem", "descricaoTipoBem", 
+                            "detalheBem", "valorBem", "dataUltimaAtualizacao", "horaUltimaAtualizacao")
+    
+    colnames(declaracao_2012) <- colunas_declaracao
+    
+    return(declaracao_2012)
 }
 
 importDecalaracao2016 <- function(dataPath){
-  colunas_declaracao <- c("dataGeracao", "horaGeracao", "anoEleicao", "descEleicao", "siglaUF", "sequencialCandidato", "codTipoBem", "descricaoTipoBem", 
-                          "detalheBem", "valorBem", "dataUltimaAtualizacao", "horaUltimaAtualizacao")
-
-  declaracao_2016 <- read_delim(dataPath, delim = ";", col_names = FALSE, col_types = "cciccciccncc",
-                                locale = locale(encoding = "latin1"))
-  
-  colnames(declaracao_2016) <- colunas_declaracao
-  
-  return(declaracao_2016)
-  
+    colunas_declaracao <- c("dataGeracao", "horaGeracao", "anoEleicao", "descEleicao", "siglaUF", "sequencialCandidato", "codTipoBem", "descricaoTipoBem", 
+                            "detalheBem", "valorBem", "dataUltimaAtualizacao", "horaUltimaAtualizacao")
+    
+    declaracao_2016 <- read_delim(dataPath, delim = ";", col_names = FALSE, col_types = "cciccciccncc",
+                                  locale = locale(encoding = "latin1"))
+    
+    colnames(declaracao_2016) <- colunas_declaracao
+    
+    return(declaracao_2016)
+    
 }
 
+importCandidatos2008 <- function(dataPath){
+    candidatos_2008 <- read_delim(dataPath, delim = ";", col_names = FALSE, 
+                                  col_types = "cciicccciccciccicicciccciccccicicicicccciic",
+                                  locale = locale(encoding = "latin1"))
+    
+    colunas_candidatos <- c("dataGeracao", "horaGeracao", "anoEleicao", "numTurno", "descEleicao",  "siglaUF", 
+                            "siglaUE", "descricaoUE", "codCargo", "descCargo", "nomeCandidato", "sequencialCandidato", 
+                            "numeroCandidato", "cpfCandidato","nomeUrnaCandidato", "codSituacaoCandidatura", "desSituacaoCandidatura", 
+                            "numeroPartido", "siglaPartido", "nomePartido", "codLegenda", "siglaLegenda", 
+                            "composicaoLegenda", "nomeLegenda", "codOcupacao", "descOcupacao", "dataNascimento", 
+                            "numTituloEleitoralCand", "idadeCandDataEleicao", "codSexo", "descSexo", 
+                            "codGrauInstrucao", "descGrauInstrucao", "codEstadoCivil", "descEstadoCivil", 
+                            "codNacionalidade", "descNacionalidade", "siglaUFNasc", "codMunicipioNasc", 
+                            "nomeMunicipioNasc", "despesaMaxCampanha", "codSituacaoTurno", "descSituacaoTurno")
+    colnames(candidatos_2008) <- colunas_candidatos
+    
+    return(candidatos_2008)
+}
+
+
 importCandidatos2012 <- function(dataPath){
-  candidatos_2012 <- read_delim(dataPath, delim = ";", col_names = FALSE,
-                                locale = locale(encoding = "latin1"),
-                                col_types = "cciccccciccccccicicccccciccccicicicicccccicc")
-  
-  colnames(candidatos_2012) <- c("dataGeracao", "horaGeracao", "anoEleicao", "numTurno", "descEleicao", "siglaUF", "siglaUnidEleitoral",
-                                 "descUnidEleitoral", "codCargo", "descCargo", "nomeCandidato", "sequencialCandidato", "numeroCandidato", 
-                                 "cpfCandidato", "nomeUrnaCandidato", "codSituacaoCandidatura", "descSituacaoCandidatura", "numeroPartido", 
-                                 "siglaPartido", "nomePartido", "codLegenda", "siglaLegenda", "composicaoLegenda", "nomeLegenda", "codOcupacao",
-                                 "descOcupacao", "dataNascimento", "numTituloEleitoralCand", "idadeCandDataEleicao", "codSexo", "descSexo", 
-                                 "codGrauInstrucao", "descGrauInstrucao", "codEstadoCivil", "descEstadoCivil", "codNacionalidade", 
-                                 "descNacionalidade", "siglaUFNasc", "codMunicipioNasc", "nomeMunicipioNasc", "despesaMaxCampanha", 
-                                 "codSituacaoEleito", "descSituacaoEleito", "email")
-  
-  return(candidatos_2012)
+    candidatos_2012 <- read_delim(dataPath, delim = ";", col_names = FALSE,
+                                  locale = locale(encoding = "latin1"),
+                                  col_types = "cciccccciccccccicicccccciccccicicicicccccicc")
+    
+    colnames(candidatos_2012) <- c("dataGeracao", "horaGeracao", "anoEleicao", "numTurno", "descEleicao", "siglaUF", "siglaUnidEleitoral",
+                                   "descUnidEleitoral", "codCargo", "descCargo", "nomeCandidato", "sequencialCandidato", "numeroCandidato", 
+                                   "cpfCandidato", "nomeUrnaCandidato", "codSituacaoCandidatura", "descSituacaoCandidatura", "numeroPartido", 
+                                   "siglaPartido", "nomePartido", "codLegenda", "siglaLegenda", "composicaoLegenda", "nomeLegenda", "codOcupacao",
+                                   "descOcupacao", "dataNascimento", "numTituloEleitoralCand", "idadeCandDataEleicao", "codSexo", "descSexo", 
+                                   "codGrauInstrucao", "descGrauInstrucao", "codEstadoCivil", "descEstadoCivil", "codNacionalidade", 
+                                   "descNacionalidade", "siglaUFNasc", "codMunicipioNasc", "nomeMunicipioNasc", "despesaMaxCampanha", 
+                                   "codSituacaoEleito", "descSituacaoEleito", "email")
+    
+    return(candidatos_2012)
 }
 
 importCandidatos2016 <- function(dataPath){
