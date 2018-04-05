2d900f65300fdba9cd3670f003590daded7f97db
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2018-04-05 18:05:30 -0300

    Script para baixar todo os dados do TSE

---
+++ data/get_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Baixando dados do TSE"
+for ano in 2012 2016; do 
+	echo "Eleição de $ano"
+	curl -o bem_candidato_${ano}.zip http://agencia.tse.jus.br/estatistica/sead/odsele/bem_candidato/bem_candidato_${ano}.zip
+	curl -o consulta_cand_${ano}.zip http://agencia.tse.jus.br/estatistica/sead/odsele/consulta_cand/consulta_cand_${ano}.zip
+done
