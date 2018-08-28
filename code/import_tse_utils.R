434983e7963948417b1a0e5cef86a59063693e03
gileadekelvin <gileadekelvin@gmail.com>
2018-08-28 15:09:14 -0300

    Corrige função de importação dos dados considerando novo formato do TSE

--- code/import_tse_utils.R
+++ code/import_tse_utils.R
@@ -20,18 +20,21 @@ importDecalaracao <- function(dataPath, ano) {
 }
 
 importDecalaracao2018 <- function(dataPath) {
-    declaracao_2018 <- read_delim(dataPath, delim = ";", col_names = TRUE, col_types = "cciccccciccncciic",
+    declaracao_2018 <- read_csv2(dataPath, col_names = TRUE, col_types = "cciiciccccccciccncc",
                              locale = locale(encoding = "latin1"))
     
-    colunas_declaracao <- c("dataGeracao", "horaGeracao", "anoEleicao", "descEleicao", 
-                            "siglaUF", "siglaUE", "descUE", "sequencialCandidato", "codTipoBem", "descricaoTipoBem", 
-                            "detalheBem", "valorBem", "dataUltimaAtualizacao", "horaUltimaAtualizacao", "codEleicao",
-                            "numOrdemBem", "dataEleicao")
+    colunas_declaracao <- c("dataGeracao", "horaGeracao", "anoEleicao", "codTipoEleicao", "descTipoEleicao", "codEleicao", "descEleicao", 
+                            "dataEleicao", "siglaUF", "siglaUE", "descUE", "sequencialCandidato", "numOrdemCandidato",
+                            "codTipoBem", "descricaoTipoBem", "detalheBem", "valorBem", "dataUltimaAtualizacao", "horaUltimaAtualizacao")
     
     colnames(declaracao_2018) <- colunas_declaracao
     
-    return(declaracao_2018 %>% 
-               select(-c("siglaUE", "descUE", "codEleicao", "numOrdemBem", "dataEleicao")))
+    declaracao_2018 <- declaracao_2018 %>% 
+        select("dataGeracao", "horaGeracao", "anoEleicao", "descEleicao", "siglaUF", 
+               "sequencialCandidato", "codTipoBem", "descricaoTipoBem", 
+               "detalheBem", "valorBem", "dataUltimaAtualizacao", "horaUltimaAtualizacao")
+    
+    return(declaracao_2018)
 }
 
 importCandidatos <- function(dataPath, ano) {
@@ -147,22 +150,32 @@ importCandidatos2016 <- function(dataPath){
 importCandidatos2018 <- function(dataPath){
     candidatos_2018 <- read_delim(dataPath, delim = ";", col_names = TRUE,
                                   locale = locale(encoding = "latin1"),
-                                  col_types = "cciicccciccccccicccccccciccciicicicicicciciiccciciiccccc")
+                                  col_types = "cciicciccccccicccccccciciccicccccicccccicicicicicicniccccc")
     
-    colunas_candidatos <- c("dataGeracao", "horaGeracao", "anoEleicao", "numTurno", "descEleicao", "siglaUF", "siglaUnidEleitoral",
-                            "descUnidEleitoral", "codCargo", "descCargo", "nomeCandidato", "sequencialCandidato", "numeroCandidato",
-                            "cpfCandidato", "nomeUrnaCandidato", "codSituacaoCandidatura", "descSituacaoCandidatura", "numeroPartido",
-                            "siglaPartido", "nomePartido", "codLegenda", "siglaLegenda", "composicaoLegenda", "nomeLegenda", "codOcupacao",
-                            "descOcupacao", "dataNascimento", "numTituloEleitoralCand", "idadeCandDataEleicao", "codSexo", "descSexo", 
-                            "codGrauInstrucao", "descGrauInstrucao", "codEstadoCivil", "descEstadoCivil", "codCorRaca", "descCorRaca",
-                            "codNacionalidade", "descNacionalidade", "siglaUFNasc", "codMunicipioNasc", "nomeMunicipioNasc", 
-                            "despesaMaxCampanha", "codSituacaoEleito", "descSituacaoEleito", "email", "dataEleicao", 
-                            "codSituacaoCandSuperior", "descSituacaoCandSuperior", "codEleicao", "codTipoEleicao", "descTipoEleicao", "situacaoReeleicao", 
-                            "tipoAbrangencia", "numProtocoloCandidatura", "numProcessoUnico")
+    colunas_candidatos <- c("dataGeracao", "horaGeracao", "anoEleicao", "codTipoEleicao", "descTipoEleicao", "numTurno", "codEleicao", "descEleicao", 
+                            "dataEleicao", "tipoAbrangencia", "siglaUF", "siglaUnidEleitoral",
+                            "descUnidEleitoral", "codCargo", "descCargo", "sequencialCandidato", "numeroCandidato", "nomeCandidato",
+                            "nomeUrnaCandidato", "nomeSocialCandidato", "cpfCandidato", "email", "codSituacaoCandidatura", "descSituacaoCandidatura", 
+                            "codDetalheSituacaoCand", "descDetalheSituacaoCand", "tipoAgremiacao", "numeroPartido", "siglaPartido", 
+                            "nomePartido", "codLegenda", "nomeLegenda", "composicaoLegenda", "codNacionalidade", "descNacionalidade",
+                            "siglaUFNasc", "codMunicipioNasc", "nomeMunicipioNasc", "dataNascimento", "idadeCandDataEleicao", "numTituloEleitoralCand",
+                            "codSexo", "descSexo", "codGrauInstrucao", "descGrauInstrucao","codEstadoCivil", "descEstadoCivil", "codCorRaca", "descCorRaca",
+                            "codOcupacao", "descOcupacao", "despesaMaxCampanha", "codSituacaoEleito", "descSituacaoEleito", "situacaoReeleicao",
+                            "situacaoDeclararBens", "numProtocoloCandidatura", "numProcesso")
     
     colnames(candidatos_2018) <- colunas_candidatos
-    return(candidatos_2018 %>% 
-               select(-c("dataEleicao", "codSituacaoCandSuperior", "descSituacaoCandSuperior", "codEleicao", "codTipoEleicao", 
-                         "descTipoEleicao", "situacaoReeleicao", "tipoAbrangencia", "numProtocoloCandidatura", 
-                         "numProcessoUnico")))
+    
+    candidatos_2018 <- candidatos_2018 %>% 
+        mutate(siglaLegenda = "#NE#") %>%
+        select("dataGeracao", "horaGeracao", "anoEleicao", "numTurno", "descEleicao",            
+               "siglaUF", "siglaUnidEleitoral", "descUnidEleitoral", "codCargo", "descCargo",
+               "nomeCandidato", "sequencialCandidato", "numeroCandidato", "cpfCandidato", "nomeUrnaCandidato",
+               "codSituacaoCandidatura", "descSituacaoCandidatura", "numeroPartido", "siglaPartido", "nomePartido",
+               "codLegenda", "siglaLegenda", "composicaoLegenda", "nomeLegenda", "codOcupacao",
+               "descOcupacao", "dataNascimento", "numTituloEleitoralCand", "idadeCandDataEleicao", "codSexo",
+               "descSexo", "codGrauInstrucao", "descGrauInstrucao", "codEstadoCivil", "descEstadoCivil",        
+               "codCorRaca", "descCorRaca", "codNacionalidade", "descNacionalidade", "siglaUFNasc",
+               "codMunicipioNasc", "nomeMunicipioNasc", "despesaMaxCampanha", "codSituacaoEleito", "descSituacaoEleito", "email")
+        
+    return(candidatos_2018)
 }
